df13e9781c855eda13f78ed7f9e585d1f754f7c4
Afroz555 <143890736+Afroz555@users.noreply.github.com>
2023-09-02 20:00:08 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,20 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+;
+function sub(num1, num2) {
+    return num1 - num2;
+}
+;
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+;
+function divide(num1, num2) {
+    return num1 / num2;
+}
+;
+export { sum };
+export { sub };
+export { multiply };
+export { divide };
